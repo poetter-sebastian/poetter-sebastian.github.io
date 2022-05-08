49547c6e668e4e5c1ad1717632dd30a04ce96ebf
poetter-sebastian <poetter-sebastian@gmx.de>
2022-05-08 14:27:27 +0200

    # Fixed console error # Fixed html error

--- src/js/pwa.js
+++ src/js/pwa.js
@@ -5,7 +5,9 @@ window.addEventListener('beforeinstallprompt', (event) => {
     // Stash the event so it can be triggered later.
     window.deferredPrompt = event;
     // Remove the 'hidden' class from the install button container.
-    divInstall.classList.toggle('hidden', false);
+    if (typeof divInstall != "undefined") {
+        divInstall.classList.toggle('hidden', false);
+    }
 });
 
 // register service worker
